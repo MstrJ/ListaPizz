0a58544a89a33e7f61695763189bebb07e91f025
MstrJ <jakubkruliczak@gmail.com>
2022-11-09 09:16:09 +0100

    Kod do poprawienia

---
+++ ProgramPizza/ProgramPizza/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Podaj pizz: (a,b,c)||(a b c)||(a;b;c) ");
+        string pizze = Console.ReadLine();
+        string[] listaPizz = pizze.Split(',', ' ', ';');
+
+
+        foreach (var item in listaPizz)
+        {
+            Console.WriteLine($"Pizza {item}");
+        }
+    }
+}
